013c9385b2c465581cbf108c108c27f463c52ec0
= <sebastiangiulianelli02@gmail.com>
2024-07-04 15:05:58 -0300

    Agrego proyeccion de tablas

--- app/Main.hs
+++ app/Main.hs
@@ -29,10 +29,17 @@ cutCols ((r:rows), tcols) cols = let r' = cutCol (r, tcols) cols
                                      rows' = cutCols (rows, tcols) cols
                                  in (r':rows')
 
+sortCols :: [Column] -> [Column] -> [Column]
+sortCols _ [] = []
+sortCols cols (c: cs) = if exist c cols then c:(sortCols cols cs)
+                                        else sortCols cols cs
+                        where exist _ [] = False
+                              exist co (col:columns) = if co == col then True
+                                                                    else exist co columns   
                             
 proyeccion :: [Column] -> Table -> Table
 proyeccion [] (_, tname, _) = ([], tname, [])  
-proyeccion cols (trows, _, tcols) = let trows' = cutCols (trows, tcols) cols -- CS TIENE QUE ESTAR ORDENADO
+proyeccion cols (trows, _, tcols) = let trows' = cutCols (trows, tcols) (sortCols cols tcols) -- CS TIENE QUE ESTAR ORDENADO
                                    in (trows', "+", cols)
 ---------------------------------------------------------
 
@@ -66,7 +73,8 @@ traduce stream = Streams.toList stream
 
 
 getColumns :: [ColumnDef] -> [String]
-getColumns c = [show (columnName x) | x <- c ]
+getColumns c = [show2 (columnName x) | x <- c ]
+               where show2 w = init (tail (show w)) 
 
 getTables :: MySQLConn -> [[MySQLValue]] -> IO [Table] -- > [(table, tname, cnames)]
 getTables _ [] = return [] 
@@ -75,6 +83,11 @@ getTables conn (l:ls) = do tableName <- extractName (l!!0)
                            table <- traduce is
                            tables <- getTables conn ls
                            return ((table, tableName, getColumns c):tables) 
+
+getTableByName :: Name -> [Table] -> Table
+getTableByName _ [] = ([], "undefined",[]) -- Undefined
+getTableByName n ((a, tname, b): ts) | n == tname = (a, tname, b)
+                                     | otherwise = getTableByName n ts
 
 arSql :: IO ()
 arSql = do
@@ -85,6 +98,7 @@ arSql = do
   rows <- traduce is -- Nombre de tablas :: [MySQLText nombreTabla]
   tables <- getTables conn rows -- tablas :: [([[MySQLValue]], String)]
   printTables tables
+  printTables [(proyeccion ["proyecto_nombre", "proyecto_id"] (getTableByName "Proyectos" tables))]
 
 
 
